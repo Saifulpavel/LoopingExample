dec3833b4578e2e706ea5fd0194d709712ed5c98
saiful Islam <saifulislam0980@gmail.com>
2021-08-06 02:23:22 +0600

    Updated

--- LoopingExample/Program.cs
+++ LoopingExample/Program.cs
@@ -1,15 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace LoopingExample
 {
     class Program
     {
         static void Main(string[] args)
         {
+            //For Loop//
+            for (int i=0;i<=10;i++)
+            {
+                Console.WriteLine("Hello");
+            }
+
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            //Even Number Using For Loop
+            for (int i = 0; i <= 10; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadKey();
         }
     }
 }
